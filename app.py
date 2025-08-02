b085e692385e76fea0affa45dc2788d5f7597255
Sandras Mac <sandra@46:f9:3b:93:80:fc.speedport.ip>
2025-08-03 00:04:39 +0200

    designed data structure and created / route

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 """Blog application """
 
-from flask import Flask
+
+import json
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def index():
     """
-    Hello World endpoint
+    Home page of the Blog application. 
+    This function reads blog posts from a JSON file and renders them on the index page.
     """
-    return "Hello, World!"
+    with open("blog_posts.json", "r", encoding="utf-8") as blog_file:
+        blog_posts = json.load(blog_file)
+    return render_template('index.html', posts=blog_posts)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
