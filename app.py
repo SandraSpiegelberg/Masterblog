0da1216a82d892d078af998c0269b57da87b19de
Sandras Mac <sandra@46:f9:3b:93:80:fc.speedport.ip>
2025-08-04 13:05:50 +0200

    add a create a new post feature, with add rout and new add.html file also expand some style code

--- app.py
+++ app.py
@@ -2,10 +2,25 @@
 
 
 import json
-from flask import Flask, render_template
+import os
+from flask import Flask, redirect, render_template, request, url_for
 
 
 app = Flask(__name__)
+
+
+def load_posts():
+    if os.path.exists("blog_posts.json"):
+        with open("blog_posts.json", "r", encoding="utf-8") as blog_file:
+            return json.load(blog_file)
+    else:
+        return []
+
+
+def save_posts(posts):
+    with open ("blog_posts.json", "w", encoding="utf-8") as blog_file:
+        json.dump(posts, blog_file, indent=4, ensure_ascii=False)
+
 
 
 @app.route('/')
@@ -14,9 +29,37 @@ def index():
     Home page of the Blog application. 
     This function reads blog posts from a JSON file and renders them on the index page.
     """
-    with open("blog_posts.json", "r", encoding="utf-8") as blog_file:
-        blog_posts = json.load(blog_file)
+    blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    """
+    Add a new blog post.
+    This function handles the logic for handling new blog posts.
+    """
+    if request.method == 'POST':
+        blog_posts = load_posts()
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.formget("msg")
+        if blog_posts:
+            post_id = len(blog_posts) + 1
+        else:
+            post_id = 1
+
+        new_post = {
+            "id" : post_id,
+            "author" : author,
+            "title" : title,
+            "content" : content 
+        }
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == "__main__":
