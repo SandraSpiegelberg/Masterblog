781d1836353e524a8a0a1104afe1ea0567d127e1
Sandras Mac <sandra@46:f9:3b:93:80:fc.speedport.ip>
2025-08-06 09:11:43 +0200

    add a delete post option

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-"""Blog application """
+"""Blog application 
+This script generates webpages with flask displaying blog posts, a form to create new posts.
+It reads all posts from a JSON file, allows the user to create, delete, update and like posts."""
 
 
 import json
@@ -10,6 +12,11 @@ app = Flask(__name__)
 
 
 def load_posts():
+    """Loads the saved posts from blog_posts.json as a list of dictionaries. 
+    each post is a dictionary. It returns the list of dictionaries 
+    if the json file exists, otherwise returns an empty list.
+    :return: list
+    """
     if os.path.exists("blog_posts.json"):
         with open("blog_posts.json", "r", encoding="utf-8") as blog_file:
             return json.load(blog_file)
@@ -18,6 +25,9 @@ def load_posts():
 
 
 def save_posts(posts):
+    """Saves all posts in blog_posts.json.
+    :param posts: list of dictionaries, each dictionary containing the information of a post.
+    """
     with open ("blog_posts.json", "w", encoding="utf-8") as blog_file:
         json.dump(posts, blog_file, indent=4, ensure_ascii=False)
 
@@ -25,9 +35,9 @@ def save_posts(posts):
 
 @app.route('/')
 def index():
-    """
-    Home page of the Blog application. 
+    """Home page of the Blog application. 
     This function reads blog posts from a JSON file and renders them on the index page.
+    :return: index.html
     """
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
@@ -35,15 +45,18 @@ def index():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-    """
-    Add a new blog post.
+    """Adds a new blog post.
     This function handles the logic for handling new blog posts.
+    It renders the form for creating a new post on the add page.
+    Also redirects back to the home page after sending the new post.
+    :return: redirection to index.html if a new post is sends otherwise add.html
     """
-    if request.method == 'POST':
+    if request.method == "POST":
         blog_posts = load_posts()
         author = request.form.get("post_author")
         title = request.form.get("post_title")
         content = request.form.get("post_message")
+
         if blog_posts:
             post_id = len(blog_posts) + 1
         else:
@@ -56,10 +69,34 @@ def add():
             "content" : content 
         }
         blog_posts.append(new_post)
+
         save_posts(blog_posts)
+
         return redirect(url_for('index'))
-    else:
-        return render_template('add.html')
+
+    return render_template('add.html')
+
+
+@app.route('/delete/<int:post_id>', methods=['GET', 'POST'])
+def delete(post_id):
+    """Deletes a current blog posts. 
+    This function finds the blog post with the given id and remove it from the list.
+    Also redirects back to the home page.
+    :param post_id: unique id number of a blog post
+    """
+    blog_posts = load_posts()
+
+    blog_posts = [post for post in blog_posts if post["id"] != post_id]
+
+    for post in blog_posts:
+        if post["id"] > post_id:
+            post["id"] -= 1
+
+    save_posts(blog_posts)
+
+    return redirect(url_for('index'))
+
+    
 
 
 if __name__ == "__main__":
